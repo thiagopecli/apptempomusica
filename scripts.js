67d05703837a4a913e300fdd9490398969888f6b
thiagopecli <thiagopecli.jjboy@gmail.com>
2024-09-13 13:22:25 -0300

    novo commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,37 @@
+const input = document.getElementById('input-busca');
+
 function botaoDeBusca() {
-    
-    console.log("opa")
+  const inputValue = input.value;
+
+  movimentoInput();
 }
+
+function fecharInput() {
+  input.style.visibility = "hidden";
+  input.style.width = "40px";
+  input.style.padding =
+    "0.5rem 0.5rem 0.5rem 2.6rem";
+  input.style.transition =
+    "all 0.5s ease-in-out 0s";
+  input.value = "";
+}
+
+function abrirInput() {
+  input.style.visibility = "visible";
+  input.style.width = "300px";
+  input.style.padding =
+    "0.5rem 0.5rem 0.5rem 3.1rem";
+  input.style.transition =
+    "all 0.5s ease-in-out 0s";
+  input.value = "";
+}
+
+function movimentoInput() {
+  const visibility = input.style.visibility;
+
+  visibility === 'hidden' ? abrirInput() : fecharInput();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fecharInput();
+});
